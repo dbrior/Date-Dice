4be21ba40a621d0fc7008b4f4ab39f5d89e1427e
Daniel Brior <djbrior@gmail.com>
2025-01-25 17:05:23 -0800

    Initial Commit

---
+++ Date Dice/Date_DiceApp.swift
@@ -0,0 +1,32 @@
+//
+//  Date_DiceApp.swift
+//  Date Dice
+//
+//  Created by Daniel Brior on 1/25/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Date_DiceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
